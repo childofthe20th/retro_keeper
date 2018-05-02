e3d1dd4fba9c847dfb34a71638a160595b1ee605
Eric Sanchez <bluemojo82@gmail.com>
2018-05-02 03:29:24 -0400

    routes.rb updated

--- retro_keeper/config/routes.rb
+++ retro_keeper/config/routes.rb
@@ -1,3 +1,22 @@
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/consoles', to: 'consoles#index'
+  get '/consoles/:id', to: 'consoles#show'
+  post '/consoles', to: 'consoles#create'
+  delete '/consoles/:id', to: 'consoles#delete'
+  put '/consoles/:id', to: 'consoles#update'
+
+  get '/games', to: 'games#index'
+  get '/games/:id', to: 'games#show'
+  post '/games', to: 'games#create'
+  delete '/games/:id', to: 'games#delete'
+  put '/games/:id', to: 'games#update'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#delete'
+  put '/users/:id', to: 'users#update'
+
 end
