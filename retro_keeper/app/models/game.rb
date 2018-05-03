dfd0a1b65d7e59ceb3d43024312f5393ef08ce54
Eric Sanchez <bluemojo82@gmail.com>
2018-05-02 20:49:59 -0400

    game model created and coded, needs testing

---
+++ retro_keeper/app/models/game.rb
@@ -0,0 +1,131 @@
+class Game
+  attr_reader :id, :title, :developer, :publisher, :region, :condition, :image, :rarity, :qty, :worth, :console_id, :description, :release_date
+
+  if(ENV["DATABASE_URL"])
+    uri = URI.parse(ENV["DATABASE_URL"])
+    DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user,uri.password)
+  else
+    DB = PG.connect(host: "localhost", port: 5432, dbname: "games")
+  end
+
+  def initialize(opts = {})
+    @id = opts["id"].to_i
+    @title = opts["title"]
+    @developer = opts["developer"]
+    @publisher = opts["publisher"]
+    @region = opts["region"]
+    @condition = opts["condition"]
+    @image = opts["image"]
+    @rarity = opts["rarity"]
+    @qty = opts["qty"].to_i
+    @worth = opts["worth"].to_i
+    @console_id = opts["console_id"].to_i
+    @description = opts["description"]
+    @release_date = opts["release_date"]
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM game;")
+    return results.map {|result| Game.new(result)}
+  end
+
+  def self.find(id)
+    results = DB.exec("SELECT * FROM games WHERE id=#{id};")
+    return Game.new(results.first)
+  end
+
+  def self.create(opts={})
+    results = DB.exec(
+      <<-SQL
+        INSERT INTO
+          games (
+            title,
+            developer,
+            publisher,
+            region,
+            condition,
+            image,
+            rarity,
+            qty,
+            worth,
+            console_id,
+            description,
+            release_date
+          )
+        VALUES (
+          '#{opts["title"]}',
+          '#{opts["developer"]}',
+          '#{opts["publisher"]}',
+          '#{opts["region"]}',
+          '#{opts["condition"]}',
+          '#{opts["image"]}',
+          '#{opts[rarity]}',
+          #{opts["qty"]},
+          #{opts["worth"]},
+          #{opts["console_id"]},
+          '#{opts["description"]}',
+          '#{opts[release_date]}'
+        )
+        RETURNING
+          id,
+          title,
+          developer,
+          publisher,
+          region,
+          condition,
+          image,
+          rarity,
+          qty,
+          worth,
+          console_id,
+          description,
+          release_date;
+      SQL
+    )
+    return Game.new(results.first)
+  end
+
+  def self.delete(id)
+    results = DB.exec("DELETE FROM games WHERE id=#{id};")
+    return { deleted: true }
+  end
+
+  def self.update(id, opts={})
+    results = DB.exec(
+      <<-SQL
+        UPDATE
+          games
+        SET
+          title='#{opts["title"]}',
+          developer='#{opts["developer"]}',
+          publisher='#{opts["publisher"]}',
+          region='#{opts["region"]}',
+          condition='#{opts["condition"]}',
+          image='#{opts["image"]}',
+          rarity='#{opts["rarity"]}',
+          qty=#{opts["qty"]},
+          worth=#{opts["worth"]},
+          console_id=#{opts["console_id"]},
+          description='#{opts["description"]}',
+          release_date='#{opts["release_date"]}'
+        WHERE
+          id=#{id}
+        RETURNING
+          id,
+          title,
+          developer,
+          publisher,
+          region,
+          condition,
+          image,
+          rarity,
+          qty,
+          worth,
+          console_id,
+          description,
+          release_date;
+      SQL
+    )
+    return Game.new(results.first)
+  end
+end
