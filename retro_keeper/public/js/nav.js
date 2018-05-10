77b30a28ad6df0cfa94c7822de78619e3619bb9e
Eric Sanchez <bluemojo82@gmail.com>
2018-05-10 17:45:27 -0400

    navbar updated and minimized and mobile-friendly

--- retro_keeper/public/js/nav.js
+++ retro_keeper/public/js/nav.js
@@ -1,49 +1,85 @@
 class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.toggleBurger = this.toggleBurger.bind(this)
+  }
+
+  toggleBurger(event) {
+    event.preventDefault();
+    let navbar = document.querySelector('.navbar');
+    let burger = document.querySelector('.navbar-burger');
+    let menu = document.querySelector('.navbar-menu');
+    navbar.classList.toggle('is-warning');
+    navbar.classList.toggle('is-transparent');
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  }
+
   render() {
     return(
-      <section className="hero is-dark is-bold is-small">
-        <div className="hero-body">
-          <div className="navbar-end">
-            <span className="navbar-item">
-              <a href="" className="button is-info is-inverted is-pulled-right">
-                <span className="icon">
-                  <i className="fab material-icons">account_box</i>
-                </span>
-                <span>Sign Up</span>
-              </a>
-            </span>
-          </div>
-          <div className="container has-text-left">
-            <p className="title is-size-1 main-title">
-              RetroKeeper
-            </p>
-            <p className="subtitle">
-              //keep track of your retro video game collection
-            </p>
+      <nav className="navbar is-warning is-fixed-top">
+        <div className="navbar-brand">
+          <a href="#" className="navbar-item">
+            <p className="title main-title">RetroKeeper</p>
+          </a>
+          <div onClick={()=>{this.toggleBurger(event)}} role="button" className="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
           </div>
         </div>
 
-        <div className="hero-foot">
-          <nav className="tabs is-boxed is-fullwidth">
-            <div className="container">
-              <ul>
-                <li className="is-active">
-                  <a href="">Home</a>
-                </li>
-                <li>
-                  <a href="">Consoles</a>
-                </li>
-                <li>
-                  <a href="">Games</a>
-                </li>
-                <li>
-                  <a href="">About</a>
-                </li>
-              </ul>
+        <div id="navMenu" className="navbar-menu">
+          <div className="navbar-start">
+            <a href="#" className="navbar-item">
+              <i className="material-icons">home</i>
+              Home
+            </a>
+            <div className="navbar-item has-dropdown is-hoverable is-unselectable">
+              <a className="navbar-link">
+                <i className="material-icons">view_list</i>
+                Collection
+              </a>
+              <div className="navbar-dropdown is-boxed">
+                <a href="#consoles" className="navbar-item">
+                  <i className="material-icons">tv</i>
+                  Consoles
+                </a>
+                <a href="#games" className="navbar-item">
+                  <i className="material-icons">gamepad</i>
+                  Games
+                </a>
+                <a className="navbar-item is-disabled" title="Coming soon...">
+                  <i className="material-icons">videogame_asset</i>
+                  Peripherals
+                </a>
+                <a href="#about" className="navbar-item">
+                  <i className="material-icons">info</i>
+                  About
+                </a>
+                <hr className="navbar-divider"/>
+                <div className="navbar-item">
+                  Version 0.7.1
+                </div>
+              </div>
             </div>
-          </nav>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="field is-grouped">
+                <p className="control">
+                  <a href="" className="button is-primary">
+                    <span className="icon">
+                      <i className="fab material-icons">account_box</i>
+                    </span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
+      </nav>
     )
   }
 }
