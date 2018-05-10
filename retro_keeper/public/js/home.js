f6235f610ed17db9a130e7592ba2e70133a2f767
Eric Sanchez <bluemojo82@gmail.com>
2018-05-10 17:45:00 -0400

    hero banner tweaked

--- retro_keeper/public/js/home.js
+++ retro_keeper/public/js/home.js
@@ -1,10 +1,12 @@
 class Home extends React.Component {
   render() {
     return(
-      <section className="section">
-        <div className="container">
-          <h1 className="title is-2">Welcome to retro_Keeper</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro eligendi maiores rerum eveniet, doloremque modi dolorem consequatur molestias aperiam saepe, reprehenderit alias in fugiat, dolorum impedit nisi voluptas earum natus.</p>
+      <section className="hero is-dark is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">//keep track of your retro gaming collection.</h1>
+            <a href="#consoles" className="button is-white is-outlined is-large">Get Started</a>
+          </div>
         </div>
       </section>
     )
