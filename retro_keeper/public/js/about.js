d5ef2234e1d0ef3227b5e005275c0e84bfc3c08d
Eric Sanchez <bluemojo82@gmail.com>
2018-05-11 02:14:40 -0400

    about section mostly done

--- retro_keeper/public/js/about.js
+++ retro_keeper/public/js/about.js
@@ -2,8 +2,24 @@ class About extends React.Component {
   render() {
     return(
       <section id="about" className="section">
-        <div className="container">
-          <h1 className="title is-2 has-text-centered">About Section</h1>
+        <div className="box has-text-centered">
+          <article className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img className="me" src="../images/Profile BW 2016.jpg" alt="Eric Sanchez"/>
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="content">
+                <p>
+                  <strong>Eric Sanchez</strong><small> Web Developer</small>
+                  <br/>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus natus harum enim ad velit nesciunt ipsa dolorum optio consequatur, ducimus iusto doloribus nihil tenetur reiciendis aliquid beatae et quos, similique.
+                </p>
+              </div>
+            </div>
+          </article>
+
         </div>
       </section>
     )
